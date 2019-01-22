5e321e8c96b878bc52819405028824448172dd9c
stampedethegoat <ben.basuni@datadoghq.com>
2019-01-22 08:46:47 -0800

    deleted the tests

--- countdown/ViewController.swift
+++ countdown/ViewController.swift
@@ -1,20 +1,34 @@
-//
-//  ViewController.swift
-//  countdown
-//
-//  Created by ben.basuni on 1/22/19.
-//  Copyright © 2019 ben.basuni. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var debtFreeLabel: UILabel!
+    @IBOutlet weak var newYorkLabel: UILabel!
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad();
+        self.view.backgroundColor = UIColor.lightGray;
+        instantiateDate();
     }
+    
+    func instantiateDate() -> () {
+        // Get current Date
+        let currentDay = Calendar.current.ordinality(of: .day, in: .year, for: Date());
+        
+        // Set Countdown Variables here
+        let newYork  = 90;
+        let debtFree = 59;
 
+        // Do Minimal Arithmetic to get Date from today
+        let daysTilDebtFree = debtFree - currentDay!;
+        let daysTilnewYork  = newYork - currentDay!;
 
+        // Align the Text
+        debtFreeLabel.textAlignment = .center;
+        newYorkLabel.textAlignment = .center;
+
+        // Set the Label's text to the Arith
+        debtFreeLabel.text = String (daysTilDebtFree);
+        newYorkLabel.text = String (daysTilnewYork);
+    }
 }
-
